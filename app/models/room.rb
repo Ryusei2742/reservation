5381baf4183e649430639f02920b1ef24bce9da2
ryusei42 <ryuseitakata27@gmail.com>
2025-01-19 01:05:06 +0900

    Create Models

---
+++ app/models/room.rb
@@ -0,0 +1,17 @@
+class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+  validates :check_in, :check_out, presence: true
+  validate :check_in_after_today, :check_out_after_check_in
+  validates :guests, numericality: { greater_than: 0 }
+
+  private
+
+  def check_in_after_today
+    errors.add(:check_in, "は本日以降の日付を指定してください") if check_in < Date.today
+  end
+
+  def check_out_after_check_in
+    errors.add(:check_out, "はチェックイン日より後の日付を指定してください") if check_out <= check_in
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "rooms#index" # 初期表示を施設一覧ページに設定
+  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :rooms do
+    resources :reservations, only: [:new, :create, :index, :show]
+  end
+  resource :session, only: [:new, :create, :destroy]
 end
